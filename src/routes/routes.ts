848ba587ced19fb6f0b09ae5eb6e429785aeb497
elmanci2 <mancillaandres7@gmail.com>
2024-12-21 20:30:07 -0500

    change controller

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,55 +1,16 @@
 import { Router } from "express";
 import controllers from "../controllers/controllers";
 
-const  routes =  Router()
-routes.get("/" , (req , res) => {
+const routes = Router()
+routes.get("/", (req, res) => {
     res.send("hello world")
-} )
-
-routes.post("/question" , async (req , res) => {
-    try {
-        const response = await controllers.question(req , res)
-        res.send({
-            response,
-            state: 200
-        })
-    } catch (error: any) {
-        res.send({
-            response: error.message,
-            state: 500
-        })
-    }
 })
 
-routes.post("/embed" , async (req , res) => {
-    try {
-        const response = await controllers.embed(req , res)
-        res.send({
-            response,
-            state: 200
-        })
-    } catch (error: any) {
-        res.send({
-            response: error.message,
-            state: 500
-        })
-    }
-})
+routes.post("/question", controllers.question)
 
-routes.post("/embeddings" , async (req , res) => {
-    try {
-        const response = await controllers.embeddings(req , res)
-        res.send({
-            response,
-            state: 200
-        })
-    } catch (error: any) {
-        res.send({
-            response: error.message,
-            state: 500
-        })
-    }
-})
+routes.post("/embed", controllers.embed)
+
+routes.post("/embeddings", controllers.embeddings)
 
 
 
